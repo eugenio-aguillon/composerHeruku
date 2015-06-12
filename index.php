25e7503717f75cee5b478f6cbe969d5da0ec513d
abelinoAr <mundoabelino@outlook.com>
2015-06-12 07:11:11 -0600

    Inicio con aplicacion a heruku

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require "vendor/autoload.php";
+
+function probando(){
+	$variable = $arrayName = array('uno' => 1, "otro" =>"lalala", "fin" => true );
+	d($variable);
+}
+
+probando();
+
+
